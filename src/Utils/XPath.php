cf73822bd6018f061c2005d336d23f7b87b25cd4
Tim van Dijen <tvdijen@gmail.com>
2022-07-15 18:37:12 +0200

    Test element sequence

---
+++ src/Utils/XPath.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SimpleSAML\CAS\Utils;
+
+use DOMNode;
+use DOMXPath;
+use SimpleSAML\CAS\Constants as C;
+
+/**
+ * Compilation of utilities for XPath.
+ *
+ * @package simplesamlphp/cas
+ */
+class XPath extends \SimpleSAML\XML\Utils\XPath
+{
+    /**
+     * Get a DOMXPath object that can be used to search for CAS elements.
+     *
+     * @param \DOMNode $node The document to associate to the DOMXPath object.
+     *
+     * @return \DOMXPath A DOMXPath object ready to use in the given document, with several
+     *   cas-related namespaces already registered.
+     */
+    public static function getXPath(DOMNode $node): DOMXPath
+    {
+        $xp = parent::getXPath($node);
+        $xp->registerNamespace('cas', C::NS_CAS);
+
+        return $xp;
+    }
+}
+
--- tests/XML/cas/AttributesTest.php
+++ tests/XML/cas/AttributesTest.php
@@ -6,6 +6,7 @@ namespace SimpleSAML\CAS\Test\XML\cas;
 
 use DOMDocument;
 use PHPUnit\Framework\TestCase;
+use SimpleSAML\CAS\Utils\XPath;
 use SimpleSAML\CAS\XML\cas\Attributes;
 use SimpleSAML\CAS\XML\cas\AuthenticationDate;
 use SimpleSAML\CAS\XML\cas\IsFromNewLogin;
@@ -58,6 +59,38 @@ final class AttributesTest extends TestCase
             $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
             strval($attributes)
         );
+    }
+
+
+    public function testMarshallingElementOrdering(): void
+    {
+        $authenticationDate = new AuthenticationDate('2015-11-12T09:30:10Z');
+        $longTerm = new LongTermAuthenticationRequestTokenUsed('true');
+        $isFromNewLogin = new IsFromNewLogin('true');
+        $document = DOMDocumentFactory::fromString(
+            '<cas:myAttribute xmlns:cas="http://www.yale.edu/tp/cas">myValue</cas:myAttribute>'
+        );
+        $myAttribute = new Chunk($document->documentElement);
+        $attributes = new Attributes($authenticationDate, $longTerm, $isFromNewLogin, [$myAttribute]);
+
+        $attributesElement = $attributes->toXML();
+
+        // Test for an authenticationDate
+        $xpCache = XPath::getXPath($attributesElement);
+        $attributesElements = XPath::xpQuery($attributesElement, './cas:authenticationDate', $xpCache);
+        $this->assertCount(1, $attributesElements);
+
+        // Test ordering of cas:attributes contents
+        /** @psalm-var \DOMElement[] $attributesElements */
+        $attributesElements = XPath::xpQuery(
+            $attributesElement,
+            './cas:authenticationDate/following-sibling::*',
+            $xpCache
+        );
+
+        $this->assertGreaterThanOrEqual(2, count($attributesElements));
+        $this->assertEquals('cas:longTermAuthenticationRequestTokenUsed', $attributesElements[0]->tagName);
+        $this->assertEquals('cas:isFromNewLogin', $attributesElements[1]->tagName);
     }
 
 
