91c8979c06f083933e96df547f21622a7d5fa397
Tim van Dijen <tvdijen@gmail.com>
2022-07-15 20:48:09 +0200

    Add AuthenticationSuccess element

--- src/XML/cas/AuthenticationSuccess.php
+++ src/XML/cas/AuthenticationSuccess.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace SimpleSAML\CAS\XML\cas;
 
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\CAS\Constants as C;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\MissingElementException;
+
 /**
  * Class for CAS authenticationSuccess
  *
@@ -13,4 +19,176 @@ class AuthenticationSuccess extends AbstractCasElement
 {
     /** @var string */
     public const LOCALNAME = 'authenticationSuccess';
+
+    /** @var \SimpleSAML\CAS\XML\cas\User */
+    protected User $user;
+
+    /** @var \SimpleSAML\CAS\XML\cas\Attributes */
+    protected Attributes $attributes;
+
+    /** @var \SimpleSAML\CAS\XML\cas\ProxyGrantingTicket|null */
+    protected ?ProxyGrantingTicket $proxyGrantingTicket = null;
+
+    /** @var \SimpleSAML\CAS\XML\cas\Proxies|null */
+    protected ?Proxies $proxies = null;
+
+
+    /**
+     * Initialize a cas:authenticationSuccess element
+     *
+     * @param \SimpleSAML\CAS\XML\cas\User $user
+     * @param \SimpleSAML\CAS\XML\cas\Attributes $attributes
+     * @param \SimpleSAML\CAS\XML\cas\ProxyGrantingTicket|null $proxyGrantingTicket
+     * @param \SimpleSAML\CAS\XML\cas\Proxies|null $proxies
+     */
+    public function __construct(
+        User $user,
+        Attributes $attributes,
+        ?ProxyGrantingTicket $proxyGrantingTicket = null,
+        ?Proxies $proxies = null
+    ) {
+        $this->setUser($user);
+        $this->setAttributes($attributes);
+        $this->setProxyGrantingTicket($proxyGrantingTicket);
+        $this->setProxies($proxies);
+    }
+
+
+    /**
+     * @return \SimpleSAML\CAS\XML\cas\User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+
+    /**
+     * @param \SimpleSAML\CAS\XML\cas\User $user
+     */
+    private function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+
+    /**
+     * @return \SimpleSAML\CAS\XML\cas\Attributes
+     */
+    public function getAttributes(): Attributes
+    {
+        return $this->attributes;
+    }
+
+
+    /**
+     * @param \SimpleSAML\CAS\XML\cas\Attributes $attributes
+     */
+    private function setAttributes(Attributes $attributes): void
+    {
+        $this->attributes = $attributes;
+    }
+
+
+    /**
+     * @return \SimpleSAML\CAS\XML\cas\ProxyGrantingTicket
+     */
+    public function getProxyGrantingTicket(): ?ProxyGrantingTicket
+    {
+        return $this->proxyGrantingTicket;
+    }
+
+
+    /**
+     * @param \SimpleSAML\CAS\XML\cas\ProxyGrantingTicket $proxyGrantingTicket
+     */
+    private function setProxyGrantingTicket(?ProxyGrantingTicket $proxyGrantingTicket): void
+    {
+        $this->proxyGrantingTicket = $proxyGrantingTicket;
+    }
+
+
+    /**
+     * @return \SimpleSAML\CAS\XML\cas\Proxies
+     */
+    public function getProxies(): ?Proxies
+    {
+        return $this->proxies;
+    }
+
+
+    /**
+     * @param \SimpleSAML\CAS\XML\cas\Proxies $proxies
+     */
+    private function setProxies(?Proxies $proxies): void
+    {
+        $this->proxies = $proxies;
+    }
+
+
+    /**
+     * Convert XML into a cas:authenticationSuccess-element
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return \SimpleSAML\CAS\XML\cas\AuthenticationSuccess
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException if the qualified name of the supplied element is wrong
+     * @throws \SimpleSAML\XML\Exception\MissingAttributeException if the supplied element is missing one of the mandatory attributes
+     */
+    public static function fromXML(DOMElement $xml): object
+    {
+        Assert::same($xml->localName, 'authenticationSuccess', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, AuthenticationSuccess::NS, InvalidDOMElementException::class);
+
+        $user = User::getChildrenOfClass($xml);
+        Assert::count(
+            $user,
+            1,
+            'Exactly one <cas:user> must be specified.',
+            MissingElementException::class
+        );
+
+        $attributes = Attributes::getChildrenOfClass($xml);
+        Assert::count(
+            $attributes,
+            1,
+            'Exactly one <cas:attributes> must be specified.',
+            MissingElementException::class
+        );
+
+        $proxyGrantingTicket = ProxyGrantingTicket::getChildrenOfClass($xml);
+        $proxies = Proxies::getChildrenOfClass($xml);
+
+        return new self(
+            array_pop($user),
+            array_pop($attributes),
+            empty($proxyGrantingTicket) ? null : array_pop($proxyGrantingTicket),
+            empty($proxies) ? null : array_pop($proxies)
+        );
+    }
+
+
+    /**
+     * Convert this AuthenticationSuccess to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this AuthenticationSuccess to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+
+        $this->user->toXML($e);
+        $this->attributes->toXML($e);
+
+        if ($this->proxyGrantingTicket !== null) {
+            $this->proxyGrantingTicket->toXML($e);
+        }
+
+        if ($this->proxies !== null) {
+            $this->proxies->toXML($e);
+        }
+
+        return $e;
+    }
 }
---
+++ tests/XML/cas/AuthenticationSuccessTest.php
@@ -0,0 +1,167 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\CAS\Test\XML\cas;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\CAS\Utils\XPath;
+use SimpleSAML\CAS\XML\cas\Attributes;
+use SimpleSAML\CAS\XML\cas\AuthenticationDate;
+use SimpleSAML\CAS\XML\cas\AuthenticationSuccess;
+use SimpleSAML\CAS\XML\cas\IsFromNewLogin;
+use SimpleSAML\CAS\XML\cas\LongTermAuthenticationRequestTokenUsed;
+use SimpleSAML\CAS\XML\cas\ProxyGrantingTicket;
+use SimpleSAML\CAS\XML\cas\Proxies;
+use SimpleSAML\CAS\XML\cas\Proxy;
+use SimpleSAML\CAS\XML\cas\User;
+use SimpleSAML\Test\XML\SerializableXMLTestTrait;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\CAS\XML\cas\AuthenticationSuccessTest
+ *
+ * @covers \SimpleSAML\CAS\XML\cas\AuthenticationSuccess
+ * @covers \SimpleSAML\CAS\XML\cas\AbstractCasElement
+ *
+ * @package simplesamlphp/cas
+ */
+final class AuthenticationSuccessTest extends TestCase
+{
+    use SerializableXMLTestTrait;
+
+    /**
+     */
+    protected function setUp(): void
+    {
+        $this->testedClass = AuthenticationSuccess::class;
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(dirname(dirname(__FILE__))) . '/resources/xml/cas_authenticationSuccess.xml'
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $firstNameDocument = DOMDocumentFactory::fromString(
+            '<cas:firstname xmlns:cas="http://www.yale.edu/tp/cas">John</cas:firstname>'
+        );
+        $firstName = new Chunk($firstNameDocument->documentElement);
+
+        $lastNameDocument = DOMDocumentFactory::fromString(
+            '<cas:lastname xmlns:cas="http://www.yale.edu/tp/cas">Doe</cas:lastname>'
+        );
+        $lastName = new Chunk($lastNameDocument->documentElement);
+
+        $emailDocument = DOMDocumentFactory::fromString(
+            '<cas:email xmlns:cas="http://www.yale.edu/tp/cas">jdoe@example.org</cas:email>'
+        );
+        $email = new Chunk($emailDocument->documentElement);
+
+        $authenticationDate = new AuthenticationDate('2015-11-12T09:30:10Z');
+        $longTerm = new LongTermAuthenticationRequestTokenUsed('true');
+        $isFromNewLogin = new IsFromNewLogin('true');
+
+        $user = new User('username');
+        $attributes = new Attributes($authenticationDate, $longTerm, $isFromNewLogin, [$firstName, $lastName, $email]);
+        $proxyGrantingTicket = new ProxyGrantingTicket('PGTIOU-84678-8a9d...');
+        $proxies = new Proxies([
+            new Proxy('https://proxy2/pgtUrl'),
+            new Proxy('https://proxy1/pgtUrl')
+        ]);
+
+        $authenticationSuccess = new AuthenticationSuccess($user, $attributes, $proxyGrantingTicket, $proxies);
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($authenticationSuccess)
+        );
+    }
+
+
+    public function testMarshallingElementOrdering(): void
+    {
+        $firstNameDocument = DOMDocumentFactory::fromString(
+            '<cas:firstname xmlns:cas="http://www.yale.edu/tp/cas">John</cas:firstname>'
+        );
+        $firstName = new Chunk($firstNameDocument->documentElement);
+
+        $lastNameDocument = DOMDocumentFactory::fromString(
+            '<cas:lastname xmlns:cas="http://www.yale.edu/tp/cas">Doe</cas:lastname>'
+        );
+        $lastName = new Chunk($lastNameDocument->documentElement);
+
+        $emailDocument = DOMDocumentFactory::fromString(
+            '<cas:email xmlns:cas="http://www.yale.edu/tp/cas">jdoe@example.org</cas:email>'
+        );
+        $email = new Chunk($emailDocument->documentElement);
+
+        $authenticationDate = new AuthenticationDate('2015-11-12T09:30:10Z');
+        $longTerm = new LongTermAuthenticationRequestTokenUsed('true');
+        $isFromNewLogin = new IsFromNewLogin('true');
+
+        $user = new User('username');
+        $attributes = new Attributes($authenticationDate, $longTerm, $isFromNewLogin, [$firstName, $lastName, $email]);
+        $proxyGrantingTicket = new ProxyGrantingTicket('PGTIOU-84678-8a9d...');
+        $proxies = new Proxies([
+            new Proxy('https://proxy2/pgtUrl'),
+            new Proxy('https://proxy1/pgtUrl')
+        ]);
+
+        $authenticationSuccess = new AuthenticationSuccess($user, $attributes, $proxyGrantingTicket, $proxies);
+        $authenticationSuccessElement = $authenticationSuccess->toXML();
+
+        // Test for a user-element
+        $xpCache = XPath::getXPath($authenticationSuccessElement);
+        $authenticationSuccessElements = XPath::xpQuery($authenticationSuccessElement, './cas:user', $xpCache);
+        $this->assertCount(1, $authenticationSuccessElements);
+
+        // Test ordering of cas:authenticationSuccess contents
+        /** @psalm-var \DOMElement[] $authenticationSuccessElements */
+        $authenticationSuccessElements = XPath::xpQuery(
+            $authenticationSuccessElement,
+            './cas:user/following-sibling::*',
+            $xpCache
+        );
+
+        $this->assertCount(3, $authenticationSuccessElements);
+        $this->assertEquals('cas:attributes', $authenticationSuccessElements[0]->tagName);
+        $this->assertEquals('cas:proxyGrantingTicket', $authenticationSuccessElements[1]->tagName);
+        $this->assertEquals('cas:proxies', $authenticationSuccessElements[2]->tagName);
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $authenticationSuccess = AuthenticationSuccess::fromXML($this->xmlRepresentation->documentElement);
+
+        $this->assertEquals('username', $authenticationSuccess->getUser()->getContent());
+
+        $attributes = $authenticationSuccess->getAttributes();
+        $this->assertEquals('true', $attributes->getIsFromNewLogin()->getContent());
+        $this->assertEquals('true', $attributes->getLongTermAuthenticationRequestTokenUsed()->getContent());
+        $this->assertCount(3, $attributes->getElements());
+
+
+        $firstNameElement = $attributes->getElements()[0]->getXML();
+        $lastNameElement = $attributes->getElements()[1]->getXML();
+        $emailElement = $attributes->getElements()[2]->getXML();
+
+        $this->assertEquals('firstname', $firstNameElement->localName);
+        $this->assertEquals('John', $firstNameElement->textContent);
+        $this->assertEquals('lastname', $lastNameElement->localName);
+        $this->assertEquals('Doe', $lastNameElement->textContent);
+        $this->assertEquals('email', $emailElement->localName);
+        $this->assertEquals('jdoe@example.org', $emailElement->textContent);
+    }
+}
