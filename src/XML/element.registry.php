403264f062c5fe8bacc042f048e0ad6a200e524d
Tim van Dijen <tvdijen@gmail.com>
2024-09-16 14:14:57 +0200

    Strip element registry of any non top-level elements

--- src/XML/element.registry.php
+++ src/XML/element.registry.php
@@ -4,19 +4,6 @@ declare(strict_types=1);
 
 return [
     'http://www.yale.edu/tp/cas' => [
-        'attributes' => '\SimpleSAML\CAS\XML\cas\Attributes',
-        'authenticationDate' => '\SimpleSAML\CAS\XML\cas\AuthenticationDate',
-        'authenticationFailure' => '\SimpleSAML\CAS\XML\cas\AuthenticationFailure',
-        'authenticationSuccess' => '\SimpleSAML\CAS\XML\cas\AuthenticationSuccess',
-        'isFromNewLogin' => '\SimpleSAML\CAS\XML\cas\IsFromNewLogin',
-        'longTermAuthenticationRequestTokenUsed' => '\SimpleSAML\CAS\XML\cas\LongTermAuthenticationRequestTokenUsed',
-        'proxies' => '\SimpleSAML\CAS\XML\cas\Proxies',
-        'proxy' => '\SimpleSAML\CAS\XML\cas\Proxy',
-        'proxyFailure' => '\SimpleSAML\CAS\XML\cas\ProxyFailure',
-        'proxyGrantingTicket' => '\SimpleSAML\CAS\XML\cas\ProxyGrantingTicket',
-        'proxySuccess' => '\SimpleSAML\CAS\XML\cas\ProxySuccess',
-        'proxyTicket' => '\SimpleSAML\CAS\XML\cas\ProxyTicket',
         'serviceResponse' => '\SimpleSAML\CAS\XML\cas\ServiceResponse',
-        'user' => '\SimpleSAML\CAS\XML\cas\User',
     ],
 ];
