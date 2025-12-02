d5d42293be159ce473e2a7d1ae69cf5c58c8595b
Tim van Dijen <tvdijen@gmail.com>
2025-12-02 21:05:28 +0100

    Refactor proxySuccess

--- src/CAS/XML/ProxySuccess.php
+++ src/CAS/XML/ProxySuccess.php
@@ -14,32 +14,8 @@ use SimpleSAML\XMLSchema\Exception\MissingElementException;
  *
  * @package simplesamlphp/xml-cas
  */
-final class ProxySuccess extends AbstractResponse
+final class ProxySuccess extends AbstractProxySuccess
 {
-    /** @var string */
-    final public const LOCALNAME = 'proxySuccess';
-
-
-    /**
-     * Initialize a cas:proxySuccess element
-     *
-     * @param \SimpleSAML\CAS\XML\ProxyTicket $proxyTicket
-     */
-    final public function __construct(
-        protected ProxyTicket $proxyTicket,
-    ) {
-    }
-
-
-    /**
-     * @return \SimpleSAML\CAS\XML\ProxyTicket
-     */
-    public function getProxyTicket(): ProxyTicket
-    {
-        return $this->proxyTicket;
-    }
-
-
     /**
      * Initialize an ProxySuccess element.
      *
@@ -65,21 +41,5 @@ final class ProxySuccess extends AbstractResponse
         );
 
         return new static($proxyTicket[0]);
-    }
-
-
-    /**
-     * Convert this ProxySuccess to XML.
-     *
-     * @param \DOMElement|null $parent The element we should append to.
-     * @return \DOMElement This ProxySuccess-element.
-     */
-    public function toXML(?DOMElement $parent = null): DOMElement
-    {
-        $e = $this->instantiateParentElement($parent);
-
-        $this->getProxyTicket()->toXML($e);
-
-        return $e;
     }
 }
